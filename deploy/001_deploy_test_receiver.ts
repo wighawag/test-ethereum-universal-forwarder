2766a31d5338c7d332ae5edc8acc60a4e8a17291
wighawag <wighawag@gmail.com>
2022-09-28 10:37:09 +0100

    format:fix

--- deploy/001_deploy_test_receiver.ts
+++ deploy/001_deploy_test_receiver.ts
@@ -2,19 +2,19 @@ import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const {deployments, getNamedAccounts} = hre;
-  const {deploy} = deployments;
+	const {deployments, getNamedAccounts} = hre;
+	const {deploy} = deployments;
 
-  const {deployer} = await getNamedAccounts();
+	const {deployer} = await getNamedAccounts();
 
-  const ForwarderRegistry = await deployments.get('ForwarderRegistry');
-  const UniversalForwarder = await deployments.get('UniversalForwarder');
+	const ForwarderRegistry = await deployments.get('ForwarderRegistry');
+	const UniversalForwarder = await deployments.get('UniversalForwarder');
 
-  await deploy('TestUniversalForwardingReceiver', {
-    from: deployer,
-    args: [ForwarderRegistry.address, UniversalForwarder.address],
-    log: true,
-  });
+	await deploy('TestUniversalForwardingReceiver', {
+		from: deployer,
+		args: [ForwarderRegistry.address, UniversalForwarder.address],
+		log: true
+	});
 };
 export default func;
 func.tags = ['TestUniversalForwardingReceiver'];
